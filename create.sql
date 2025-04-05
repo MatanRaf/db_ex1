cf2e5c9c58ed5e18b65deb48edfb9dd71a454590
matan <matan.baruch@mail.huji.ac.il>
2025-04-05 13:48:53 +0300

    DONE

--- create.sql
+++ create.sql
@@ -1,16 +1,16 @@
 
 create table country_entity(
     countrycode char(3) PRIMARY KEY,
-    country     varchar not null,
-    region      varchar not null,
-    incomegroup varchar not null
+    country     varchar(50) not null,
+    region      varchar(50) not null,
+    incomegroup varchar(50) not null
 );
 
 create table university(
 	iau_id1 varchar(32) primary key,
     countrycode char(3) not null,
-	eng_name varchar not null,
-	orig_name varchar not null,
+	eng_name varchar(100) not null,
+	orig_name varchar(100) not null,
 	foundedyr integer not null,
 	private01 boolean not null,
 	latitude double precision,
@@ -19,7 +19,8 @@ create table university(
 	divisions integer,
 	specialized boolean not null,
     foreign key (countrycode)
-    	references country_entity(countrycode)
+    	references country_entity(countrycode),
+    check(foundedyr < 2026)
 );
 
 create table Enrollment_year(
@@ -28,7 +29,8 @@ create table Enrollment_year(
     students5_estimated integer,
     foreign key (iau_id1)
         references university(iau_id1),
-    PRIMARY KEY(year,iau_id1)
+    PRIMARY KEY(year,iau_id1),
+    check(year < 2026)
 );
 
 
@@ -36,5 +38,6 @@ create table closed_university(
     iau_id1 varchar(32) primary key,
 	yr_closed integer not null,
 	foreign key (iau_id1)
-	    references university(iau_id1)
+	    references university(iau_id1),
+    check(year < 2026),
 );
